edf6a46b7710b72e7df3a0bee22123f1696cb576
Gant <GantMan@gmail.com>
2015-12-23 23:28:13 -0600

    adding api files

---
+++ App/Api/Client.js
@@ -0,0 +1,47 @@
+'use strict'
+
+import Superagent from 'superagent'
+import Response from './Response'
+
+const ENDPOINT = 'https://www.wheredatcrawfish.com/'
+const API_VERSION = null
+
+export default class Client {
+
+  constructor (subdomain = '') {
+    this.subdomain = subdomain
+  }
+
+  get fullDomain () {
+    let uri = ENDPOINT
+    if (API_VERSION) {
+      uri = `${uri}/${API_VERSION}/`
+    }
+
+    return uri
+  }
+
+  get (path, qsv, callback) {
+    Superagent
+      .get(this.fullDomain + path)
+      .query(qsv || {})
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        let response = new Response(err, res)
+        callback(response)
+      })
+  }
+
+  post (path, postData, callback) {
+    Superagent
+      .post(this.fullDomain + path)
+      .send(postData || {})
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .end((err, res) => {
+        const response = new Response(err, res)
+        callback(response)
+      })
+  }
+
+}
