1cf01736ed5e85baea768d0bc747cbd0623e26f9
fvonhoven <fvonhoven@gmail.com>
2015-12-23 23:57:32 -0600

    The DetailsScreen & Style

---
+++ App/Screens/DetailsScreen.js
@@ -0,0 +1,76 @@
+'use strict'
+
+import React, { ScrollView, View, TouchableHighlight, Image, Text, TextInput } from 'react-native'
+// import { Routes } from '../Navigation/'
+import styles from '../Styles/DetailsScreenStyle'
+
+export default class DetailsScreen extends React.Component {
+
+  static propTypes = {
+    navigator: React.PropTypes.object,
+    dispatch: React.PropTypes.func
+  }
+
+  render () {
+    return (
+      <ScrollView>
+        <Image style={styles.backgroundImage} source={require('../Images/lightWood.jpg')}/>
+        <View style={styles.mapContainer}>
+          <Image style={styles.image} source={require('../Images/newspaper.jpg')}/>
+        </View>
+
+        <View style={styles.detailsContainer}>
+          <View style={styles.headerContainer}>
+            <Text allowFontScaling={false} style={styles.header}>DETAILS</Text>
+          </View>
+          <View style={styles.labelContainer}>
+            <Text allowFontScaling={false} style={styles.boldLabel}>Address</Text>
+            <Text allowFontScaling={false} style={styles.dataLabel}>HELLO</Text>
+          </View>
+          <View style={styles.labelContainer}>
+            <Text allowFontScaling={false} style={styles.boldLabel}>Phone</Text>
+            <Text allowFontScaling={false} style={styles.dataLabel}>HELLO</Text>
+          </View>
+          <View style={styles.labelContainer}>
+            <Text allowFontScaling={false} style={styles.boldLabel}>Email</Text>
+            <Text allowFontScaling={false} style={styles.dataLabel}>HELLO</Text>
+          </View>
+          <View style={styles.labelContainer}>
+            <Text allowFontScaling={false} style={styles.boldLabel}>Website</Text>
+            <Text allowFontScaling={false} style={styles.dataLabel}>HELLO</Text>
+          </View>
+
+          <View style={styles.line}/>
+
+          <TextInput
+            style={styles.discrepancyContainer}
+            placeholder='Enter New Price'
+            placeholderColor='red'
+            textAlign='center'
+          />
+          <TouchableHighlight
+            style={styles.submitButton}
+            underlayColor='red'
+          >
+            <Text allowFontScaling={false} style={{color: 'white', fontSize: 16}}>Submit</Text>
+          </TouchableHighlight>
+
+        </View>
+      </ScrollView>
+    )
+  }
+}
+
+// function mapStateToProps (state) {
+//   if (state.cases.subjectList === undefined) {
+//     return {
+//       caseId: state.cases.currentCaseId
+//     }
+//   }
+//   return {
+//     caseId: state.cases.currentCaseId,
+//     subjectList: state.cases.subjectList
+//   }
+// }
+
+// export default connect(mapStateToProps)(SubjectsScreen)
---
+++ App/Styles/DetailsScreenStyle.js
@@ -0,0 +1,105 @@
+import { StyleSheet } from 'react-native'
+import { Metrics, Colors, Fonts } from '../Themes/'
+
+export default StyleSheet.create({
+  headerContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+    borderBottomWidth: 0.5,
+    borderColor: Colors.white,
+    width: Metrics.screenWidth - 40,
+    alignSelf: 'center'
+  },
+  header: {
+    alignSelf: 'center',
+    fontSize: Metrics.fonts.normal,
+    fontFamily: Fonts.emphasis,
+    color: Colors.white,
+    paddingBottom: 5
+  },
+  boldLabel: {
+    flex: 1,
+    fontSize: Metrics.fonts.medium,
+    fontFamily: Fonts.bold,
+    color: Colors.white
+  },
+  dataLabel: {
+    flex: 2,
+    fontSize: Metrics.fonts.medium,
+    fontFamily: Fonts.base,
+    color: Colors.white
+  },
+  mapContainer: {
+    flex: 1,
+    marginTop: Metrics.navBarHeight,
+    height: 200,
+    width: Metrics.screenWidth - 20,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    shadowColor: Colors.black,
+    shadowOffset: {
+      height: 5,
+      width: 5
+    },
+    shadowOpacity: 0.55,
+    shadowRadius: 2
+  },
+  detailsContainer: {
+    backgroundColor: Colors.charcoal,
+    marginTop: 30,
+    marginBottom: 30,
+    paddingBottom: 20,
+    width: Metrics.screenWidth - 20,
+    alignSelf: 'center',
+    borderRadius: 5,
+    shadowColor: Colors.black,
+    shadowOffset: {
+      height: 7,
+      width: 7
+    },
+    shadowOpacity: 0.55,
+    shadowRadius: 2
+  },
+  line: {
+    alignSelf: 'center',
+    marginTop: 20,
+    backgroundColor: Colors.lightGray,
+    width: Metrics.screenWidth - 40,
+    height: 0.5
+  },
+  image: {
+    flex: 1,
+    width: Metrics.screenWidth - 20
+  },
+  backgroundImage: {
+    flex: 1,
+    position: 'absolute',
+    width: Metrics.screenWidth
+  },
+  labelContainer: {
+    alignItems: 'center',
+    paddingBottom: 5,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    flexDirection: 'row'
+  },
+  discrepancyContainer: {
+    paddingLeft: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignSelf: 'center',
+    width: Metrics.screenWidth - 40,
+    height: 40,
+    backgroundColor: Colors.lightGray
+  },
+  submitButton: {
+    justifyContent: 'center',
+    height: 40,
+    marginTop: 50,
+    marginHorizontal: 10,
+    alignItems: 'center',
+    borderColor: Colors.white,
+    borderRadius: 5,
+    borderWidth: 1
+  }
+})
