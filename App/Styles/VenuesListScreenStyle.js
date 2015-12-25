02a497d48666880d7dbf2f6a53f30169ef382c5b
Gant <GantMan@gmail.com>
2015-12-25 09:11:37 -0600

    styling up the navigation - charcoal

--- App/Styles/VenuesListScreenStyle.js
+++ App/Styles/VenuesListScreenStyle.js
@@ -14,7 +14,7 @@ export default StyleSheet.create({
   },
   listy: {
     marginBottom: 20,
-    marginTop: Metrics.navBarHeight
+    marginTop: Metrics.navBarHeight + 10
   },
   backgroundImage: {
     flex: 1,
