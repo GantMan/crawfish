a40e0c7122f17efa25300be8a3586dd7b276bca7
Gant <GantMan@gmail.com>
2015-12-25 09:11:18 -0600

    adding back button yayyy

--- App/Navigation/NavigationBarRouteMapper.js
+++ App/Navigation/NavigationBarRouteMapper.js
@@ -1,11 +1,23 @@
 'use strict'
 
-import React, { Text } from 'react-native'
+import React, { Text, TouchableOpacity } from 'react-native'
+import styles from '../Styles/NavigationStyle'
 
 export default {
   LeftButton (route, navigator, index, navState) {
     if (navState.leftButton) {
       return navState.leftButton
+
+    // This is dirty but currently needs to be done due to NavBar limitations
+    } else if (index > 0 && route.title !== 'Dashboard') {
+      return (
+        <TouchableOpacity
+        onPress={() => navigator.pop()}
+        style={styles.backButton}
+        >
+          <Text style={styles.backButtonText}>Back</Text>
+        </TouchableOpacity>
+      )
     }
   },
 
@@ -17,7 +29,7 @@ export default {
 
   Title (route, navigator, index, navState) {
     return (
-      <Text allowFontScaling={false}>
+      <Text allowFontScaling={false} style={styles.navTitle}>
         {navState.title || route.title}
       </Text>
     )
