7d4d0c21d1dbc116f893443e80b5b75a11db0983
fvonhoven <fvonhoven@gmail.com>
2015-12-30 15:17:03 -0600

    Fix stars, add city to props & use, condense favorite icon rendering methodology, general cleanup

--- App/Components/VenueCell.js
+++ App/Components/VenueCell.js
@@ -2,7 +2,6 @@
 
 import React, { View, Text, Image, TouchableOpacity } from 'react-native'
 import Images from '../Themes'
-import { Routes } from '../Navigation/'
 import {Icon} from 'react-native-icons'
 // import _ from 'lodash'
 
@@ -10,102 +9,80 @@ import styles from '../Styles/VenueCellStyle'
 
 export default class VenueCell extends React.Component {
 
+  // TODO Gant add ability to update favorite bool
+  // in fixture data
+
+  makeFavorite (title) {
+    window.alert(`${title} \nYou're my Favorite`)
+  }
+
+  renderIcon () {
+    if (this.props.favorite) {
+      return (
+        <Icon
+          name={'fontawesome|star'}
+          size={20}
+          color={'yellow'}
+          style={{width: 20, height: 20, marginTop: 0, marginLeft: 0}}
+        />
+      )
+    } else {
+      return (
+        <Icon
+          name={'fontawesome|star-o'}
+          size={20}
+          color={'yellow'}
+          style={{width: 20, height: 20, marginTop: 0, marginLeft: 0}}
+        />
+      )
+    }
+  }
+
   static propTypes = {
     title: React.PropTypes.string,
     imageUri: React.PropTypes.string,
     ratingUrl: React.PropTypes.string,
-    subtitle: React.PropTypes.string,
+    address: React.PropTypes.string,
+    city: React.PropTypes.string,
     distance: React.PropTypes.string,
     priceBoiled: React.PropTypes.string,
     pressDirections: React.PropTypes.func,
     navigator: React.PropTypes.object,
     favorite: React.PropTypes.bool
   }
-  // TODO Gant add ability to update favorite bool
-  // in fixture data
-
-  pressDirections () {
-    window.alert('HELLOOOOOO')
-    this.props.navigator.push(Routes.DetailsScreen)
-  }
-
-  shouldComponentUpdate (newProps, newState) {
-    // return !_.isEqual(this.state, newState) || !_.isEqual(this.props, newProps)
-  }
-
-  makeFavorite (title) {
-    window.alert(`${title} \nYou're my Favorite`)
-  }
 
   render () {
-    if (this.props.favorite) {
-      return (
-        <View style={styles.container}>
-          <View style={styles.backgroundContainer}>
-            <Image style={styles.myImage} source={{uri: this.props.imageUri}} defaultSource={Images.logo}/>
-            <View style={styles.detailsContainer}>
-              <Text allowFontScaling={false} style={styles.priceBoiled}>{this.props.priceBoiled}</Text>
-              <Image style={styles.stars} resizeMode='contain' source={{uri: this.props.ratingUrl}}/>
-            </View>
-          </View>
-          <View style={styles.overlayContainer}>
-            <View style={styles.labelsContainer}>
-              <Text allowFontScaling={false} style={styles.title}>{this.props.title}</Text>
-              <Text allowFontScaling={false} numberOfLines={0} style={styles.subtitle}>{this.props.subtitle}</Text>
-            </View>
-            <Text allowFontScaling={false} style={styles.distance}>{this.props.distance}</Text>
-          </View>
-          <View style={styles.triangleCorner}>
-            <TouchableOpacity
-              style={styles.starButton}
-              onPress={this.makeFavorite.bind(this, this.props.title)}
-              >
-              <View style={styles.icon}>
-                <Icon
-                  name={'fontawesome|star'}
-                  size={22}
-                  color={'white'}
-                  style={{width: 20, height: 20, marginTop: 0, marginLeft: 0}}
-                />
-              </View>
-            </TouchableOpacity>
+    return (
+      <View style={styles.container}>
+        <View style={styles.backgroundContainer}>
+          <Image style={styles.myImage} source={{uri: this.props.imageUri}} defaultSource={Images.logo}/>
+          <View style={styles.detailsContainer}>
+            <Text allowFontScaling={false} style={styles.priceBoiled}>{this.props.priceBoiled}</Text>
+            <Image style={styles.ratingStars} resizeMode='contain' source={{uri: this.props.ratingUrl}}/>
           </View>
         </View>
-      )
-    } else {
-      return (
-        <View style={styles.container}>
-          <View style={styles.backgroundContainer}>
-            <Image style={styles.myImage} source={{uri: this.props.imageUri}} defaultSource={Images.logo}/>
-            <View style={styles.detailsContainer}>
-              <Text allowFontScaling={false} style={styles.priceBoiled}>{this.props.priceBoiled}</Text>
-              <Image style={styles.stars} resizeMode='contain' source={{uri: this.props.ratingUrl}}/>
-            </View>
+
+        <View style={styles.overlayContainer}>
+          <View style={styles.labelsContainer}>
+            <Text allowFontScaling={false} style={styles.title}>{this.props.title}</Text>
+            <Text allowFontScaling={false} numberOfLines={0} style={styles.address}>{this.props.address} • {this.props.city}</Text>
           </View>
-          <View style={styles.overlayContainer}>
-            <View style={styles.labelsContainer}>
-              <Text allowFontScaling={false} style={styles.title}>{this.props.title}</Text>
-              <Text allowFontScaling={false} numberOfLines={0} style={styles.subtitle}>{this.props.subtitle}</Text>
-            </View>
+          <View style={styles.distanceContainer}>
             <Text allowFontScaling={false} style={styles.distance}>{this.props.distance}</Text>
           </View>
-          <View style={styles.triangleCorner}>
-            <TouchableOpacity
-              style={styles.starButton}
-              onPress={this.makeFavorite.bind(this, this.props.title)}
-              >
-              <View style={styles.icon}>
-                <Icon
-                  name={'fontawesome|star-o'}
-                  size={22}
-                  color={'white'}
-                  style={{width: 20, height: 20, marginTop: 0, marginLeft: 0}}
-                />
-              </View>
-            </TouchableOpacity>
-          </View>
         </View>
-      )
-    }
+
+        <View style={styles.triangleCorner}>
+          <TouchableOpacity
+            style={styles.starButton}
+            onPress={this.makeFavorite.bind(this, this.props.title)}
+            >
+            <View style={styles.iconContainer}>
+              {this.renderIcon()}
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 }
--- App/Styles/VenueCellStyle.js
+++ App/Styles/VenueCellStyle.js
@@ -29,32 +29,12 @@ export default StyleSheet.create({
     borderRightColor: 'transparent',
     borderTopColor: Colors.darkRed
   },
-  icon: {
+  iconContainer: {
     flex: 1,
     position: 'absolute',
     backgroundColor: 'clear',
     bottom: 27,
-    left: 10
-  },
-  favStarSelected: {
-    flex: 1,
-    position: 'absolute',
-    backgroundColor: 'clear',
-    bottom: 27,
-    left: 10,
-    color: Colors.white,
-    fontSize: 18,
-    fontFamily: Fonts.bold
-  },
-  favStarUnselected: {
-    flex: 1,
-    position: 'absolute',
-    backgroundColor: 'clear',
-    bottom: 27,
-    left: 10,
-    color: Colors.transBlack,
-    fontSize: 18,
-    fontFamily: Fonts.bold
+    left: 6
   },
   starButton: {
     top: 0,
@@ -86,18 +66,12 @@ export default StyleSheet.create({
     fontFamily: Fonts.bold,
     justifyContent: 'center'
   },
-  leftOverlay: {
-    backgroundColor: Colors.transBlack,
-    height: 25,
-    top: 0,
-    left: 0,
-    position: 'absolute'
-  },
-  stars: {
+  ratingStars: {
     height: 25,
     width: 70
   },
   overlayContainer: {
+    flex: 10,
     flexDirection: 'row',
     backgroundColor: Colors.transBlack,
     height: 40,
@@ -108,22 +82,28 @@ export default StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center'
   },
+  labelsContainer: {
+    flex: 8
+  },
+  distanceContainer: {
+    flex: 3
+  },
   title: {
     paddingLeft: 10,
     color: Colors.white,
     fontSize: 16,
     fontFamily: Fonts.bold
   },
-  subtitle: {
+  address: {
     paddingLeft: 10,
     color: Colors.white,
     fontSize: 14,
     fontFamily: Fonts.base
   },
   distance: {
-    paddingRight: 20,
     color: Colors.white,
     fontSize: 14,
-    fontFamily: Fonts.base
+    fontFamily: Fonts.base,
+    alignSelf: 'center'
   }
 })
